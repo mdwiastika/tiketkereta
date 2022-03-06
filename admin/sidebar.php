e89e9fb25e8daa8583cc30d14220a8cce9794075
mdwiastika <marceldwias@gmail.com>
2022-03-06 20:41:04 +0700

    first commit

---
+++ admin/sidebar.php
@@ -0,0 +1,112 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminMarCell</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Marcel Dwi Astika</a>
+            </div>
+        </div>
+
+        <!-- SidebarSearch Form -->
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+                <li class="nav-item active">
+                    <a href="index.php" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a href="datastasiun.php" class="nav-link">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                            Daftar Stasiun
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="datakereta.php" class="nav-link">
+                        <i class="nav-icon fas fa-plus-square"></i>
+                        <p>
+                            Daftar Kereta
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="jadwalkereta.php" class="nav-link">
+                        <i class="nav-icon fas fa-stream"></i>
+                        <p>
+                            Jadwal Kereta
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="datatiket.php" class="nav-link">
+                        <i class="nav-icon fas fa-calendar-plus"></i>
+                        <p>
+                            Data Tiket
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a href="daftarstok.php" class="nav-link">
+                        <i class="nav-icon fas fa-layer-group"></i>
+                        <p>
+                            Daftar Stok
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a href="deskripsi.php" class="nav-link">
+                        <i class="nav-icon fas fa-tags"></i>
+                        <p>
+                            Deskripsi
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a href="datapembelian.php" class="nav-link">
+                        <i class="nav-icon fas fa-clipboard-list"></i>
+                        <p>
+                            Daftar Pembelian
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a href="registrasi.php" class="nav-link">
+                        <i class="nav-icon fas fa-user-plus"></i>
+                        <p>
+                            Registrasi
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a href="logout.php" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>
+                            Log Out
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
